a1f0073f1d43ba67281f6c38678131fe8cd8de71
Thai <thai@tran.com>
2016-05-04 10:55:15 +0200

    Bank BankAccount

---
+++ Bank/src/at/spengergasse/bank/domain/Bank.java
@@ -0,0 +1,7 @@
+package at.spengergasse.bank.domain;
+
+import java.util.HashMap;
+
+public class Bank {
+	private HashMap accounts;
+}
---
+++ Bank/src/at/spengergasse/bank/domain/BankAccount.java
@@ -0,0 +1,6 @@
+package at.spengergasse.bank.domain;
+
+public class BankAccount {
+	private String 	name;
+	private int		age;
+}
